77052583f6184a08132c3a6196b312450c99d6a5
Funiba Neba <134973758+fneba@users.noreply.github.com>
2023-08-24 11:06:23 -0400

    updates

--- gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -1,5 +1,35 @@
 package com.company.gamestore.service;
 
+
+import com.company.gamestore.models.Invoice;
+import com.company.gamestore.repositories.ConsoleRepository;
+import com.company.gamestore.repositories.GameRepository;
+import com.company.gamestore.repositories.InvoiceRepository;
+import com.company.gamestore.viewmodel.InvoiceViewModel;
+import com.company.gamestore.repositories.TshirtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceLayer {
 
+    private TshirtRepository tshirtRepository;
+
+    private ConsoleRepository consoleRepository;
+
+    private GameRepository gameRepository;
+
+    private InvoiceRepository invoiceRepository;
+
+    // hardwire repos (we'll need all of them)
+    @Autowired
+    public ServiceLayer (TshirtRepository tshirtRepository, ConsoleRepository consoleRepository,
+                         GameRepository gameRepository, InvoiceRepository invoiceRepository){
+        this.tshirtRepository = tshirtRepository;
+        this.consoleRepository = consoleRepository;
+        this.gameRepository = gameRepository;
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    
 }
--- gamestore/src/test/java/com/company/gamestore/service/ServiceLayerTest.java
+++ gamestore/src/test/java/com/company/gamestore/service/ServiceLayerTest.java
@@ -1,6 +1,9 @@
 package com.company.gamestore.service;
 
+
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class ServiceLayerTest {
 
